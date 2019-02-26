e90904a39d8b14f106f936008d794b8ea84b744c
Sonia Szeton <sonia.szeton@gmail.com>
2019-02-25 19:51:31 -0700

    writing to file and sending to bucket

--- notoneth.py
+++ notoneth.py
@@ -1,8 +1,12 @@
 from twython import Twython
 import twittercredentials
 import json
+import time
+import boto3
 
 twitter = Twython(twittercredentials.CONSUMER_KEY, twittercredentials.CONSUMER_SECRET, twittercredentials.ACCESS_TOKEN, twittercredentials.ACCESS_TOKEN_SECRET)
+
+s3 = boto3.resource('s3')
 
 #results = twitter.cursor(twitter.search, q='wholesome', count=1, lang='en')
 #results = twitter.cursor(twitter.search, q='puppy', count=1, lang='en', since_id=1097719009808662529)
@@ -19,10 +23,11 @@ def getInitialID(keyword):
 def getTweets(keyword, tweetID):
     counter = 0
     results = twitter.search(q=keyword, lang='en', since_id=tweetID)
-    for result in results['statuses']:
-        tweet_text = result['text']
-        print 'Tweet Text: ', tweet_text
-        counter += 1
+    try:
+        for result in results['statuses']:
+            tweet_text = result['text']
+            print 'Tweet Text: ', tweet_text
+            counter += 1
     except TwythonError as e:
         print(e)
     return counter
@@ -39,4 +44,29 @@ def getTweets(keyword, tweetID):
 #
 #print(wholesomecount)
 
-getInitialID('wholesome')
+
+#IF 1 tweet = 1 call, maybe eliminate RT to cut back?
+
+
+totalwholesome = 0
+totalpuppy = 0
+wholesomeID = getInitialID('wholesome')
+puppyID = getInitialID('puppy')
+
+while True:
+    time.sleep(10)
+    totalwholesome += getTweets('wholesome', wholesomeID)
+    print 'Wholesome Count: ', totalwholesome
+
+    #write to file in s3 bucket
+    f = open("tweetcounter.txt", "w")
+    f.write(str(totalwholesome))
+    data = open('tweetcounter.txt', 'rb')
+    s3.Bucket('szeton-capstone').put_object(Key='tweetcounter.txt', Body=data)
+
+    totalpuppy += getTweets('puppy', puppyID)
+    print 'Puppy Count: ', totalpuppy
+    #write to file in s3 bucket
+
+    #maybe need to update the tweet ID so no repeats? Unsure.
+    #wholesomeID = getInitialID('wholesome')
